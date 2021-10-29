12de17225eb384e3ef5a4b7d975059addd2b3e62
CyrielleGl <cyrielle.gallou@gmail.com>
2021-10-29 20:01:53 +0200

    update patterns

--- src/main/webapp/app/shop/patterns/patterns.component.ts
+++ src/main/webapp/app/shop/patterns/patterns.component.ts
@@ -15,13 +15,16 @@ export class PatternsComponent implements OnInit {
   ngOnInit(): void {
     this.httpClient.get<any>('content/data/patterns.json').subscribe((data: any) => {
       this.patterns = data.patterns;
-      console.warn(this.patterns);
     });
   }
 
   downloadPdf(): void {
-    const pdfUrl = './content/patterns/kd-pattern-jules.pdf';
-    const pdfName = 'kd_pattern_jules';
+    let pdfUrl = '';
+    let pdfName = '';
+    this.patterns.map((pattern: any) => {
+      pdfUrl = pattern.linkPdf;
+      pdfName = pattern.pdfName;
+    });
     FileSaver.saveAs(pdfUrl, pdfName);
   }
 }
